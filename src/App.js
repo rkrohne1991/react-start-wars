4e300c14c8f82725e14730ab1af2492864e0afb6
Rafal Krohne <rafal.krohne@hotmail.co.uk>
2022-01-14 20:01:04 +0100

    Adding main section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./components/Header/header";
+import MainSection from "./components/MainSection/MainSection";
 import Footer from "./components/Footer/Footer";
 
 import classes from "./App.module.scss";
@@ -8,6 +9,7 @@ const App = () => {
   return (
     <div className={classes.mainApp}>
       <Header />
+      <MainSection />
       <Footer />
     </div>
   );
---
+++ src/components/MainSection/MainSection.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Fragment } from "react";
+
+import classes from "./MainSection.module.scss";
+
+const MainSection = () => {
+  return (
+    <Fragment>
+      <main className={classes.mainSection}>MAIN SECTION</main>
+    </Fragment>
+  );
+};
+
+export default MainSection;
