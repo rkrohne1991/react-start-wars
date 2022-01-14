549ae9e6deead8e90bdd56ea287cba3c7cce2910
Rafal Krohne <rafal.krohne@hotmail.co.uk>
2022-01-14 14:13:48 +0100

    Moving dependecies and adding sass

--- webpack.common.js
+++ webpack.common.js
@@ -29,6 +29,20 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
     ],
   },
 };
