e5e79ef73753135ffb58e64d3cd21813057638b0
Rafal Krohne <rafal.krohne@hotmail.co.uk>
2022-01-17 10:38:16 +0100

    Adding button toggle

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 import arrowOpen from "../../assets/arrow-open.svg";
+import arrowClose from "../../assets/arrow-close.svg";
 import classes from "./Movie.module.scss";
 
 const Movie = (props) => {
+  const [contentIsActive, setContentIsActive] = useState(false);
+
+  const toggleContentHandler = () => {
+    setContentIsActive(!contentIsActive);
+    console.log("clicked");
+  };
+
+  const btnClasses = `${classes["movie__button"]} ${
+    contentIsActive ? classes["movie__button-active"] : ""
+  }`;
+
   return (
     <li className={classes.movie}>
       <div className={classes["movie__header"]}>
         <div className={classes["movie__title"]}>{props.title}</div>
-        <button className={classes["movie__button"]}>
-          <img className={classes.logo} src={arrowOpen} alt="Open Modal" />
+        <button onClick={toggleContentHandler} className={btnClasses}>
+          <img
+            className={classes.logo}
+            src={!contentIsActive ? arrowOpen : arrowClose}
+            alt="Open Modal"
+          />
         </button>
       </div>
+      {contentIsActive && (
+        <div className={classes["movie__content"]}>Movie content</div>
+      )}
     </li>
   );
 };
